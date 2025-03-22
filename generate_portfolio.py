356cd611d5cbb61b38bdfb2fea7c9c1c339cc45a
AshishBChauhan <chauhanashi@yahoo.com>
2025-03-22 06:03:43 +0530

    template init

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -19,12 +19,10 @@ if "social_links" in data:
 
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader("."), autoescape=True)
-index_template = env.get_template("index_template.html")
-resume_template = env.get_template("resume_template.html")
+resm_template = env.get_template("resm_template.html")
 
 # Render the template with the data
-html_output = index_template.render(**data)
-resume_output = resume_template.render(**data)
+html_output = resm_template.render(**data)
 
 # This is equivalent to...
 # html_output = index_template.render(name=data["name"], label=data["label"]...)
@@ -32,7 +30,7 @@ resume_output = resume_template.render(**data)
 
 # Write the output to an HTML file
 
-with Path("resume.html").open("w", encoding="utf-8") as f:
-    f.write(resume_output)
+with Path("resm_template.html").open("w", encoding="utf-8") as f:
+    f.write(html_output)
 
 print("HTML file generated successfully!")
